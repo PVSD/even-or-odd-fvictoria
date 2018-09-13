27f703b492f1727e964808d7f111fabc4752774a
fvictoria <felipevictoria630@yahoo.com>
2018-09-13 17:42:48 -0400

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter an integer");
+        Scanner kbReader = new Scanner(System.in);
+        int n = kbReader.nextInt();
+        if (n % 2 ==0) {
+            System.out.println("Integer "+n+" is even");
+        }
+        else
+        {
+            System.out.println("Integer "+n+" is odd");
+        }
+
+
+
     }
 }
+
